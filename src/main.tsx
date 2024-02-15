18530611661938fccd4806faf7ec3efcd1a701ef
Konstantin <kapurelloruru@gmail.com>
2024-02-15 08:31:48 +0100

    feat: added styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
+import './styles/index.scss'
 
 import { App } from './App'
 ReactDOM.createRoot(document.getElementById('root')!).render(
