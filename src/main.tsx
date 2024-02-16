0abe7a767d80ebe82dff8c877f93e33e8e48a6da
chat <mr_darcy_1@mail.ru>
2024-02-16 13:30:22 +0600

    feat: add TextField component, stories and styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 import ReactDOM from 'react-dom/client'
 
+import './styles/index.scss'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
-import './styles/index.scss'
 
 import { App } from './App'
 ReactDOM.createRoot(document.getElementById('root')!).render(
