202b99843b7f064c51c0c34c7c0be5988195dca6
chat <mr_darcy_1@mail.ru>
2024-04-09 08:33:52 +0600

    feat: adding page protection

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from '@/router'
+
 export function App() {
-  return <div>Hello</div>
+  return <RouterProvider router={router} />
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,27 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-
-const router = createBrowserRouter([
+import { Navigate, Outlet, RouteObject, createBrowserRouter } from 'react-router-dom'
+const publicRoutes: RouteObject[] = [
+  {
+    element: <div>Login</div>,
+    path: '/login',
+  },
+]
+const privateRoutes: RouteObject[] = [
   {
     element: <div>Hello</div>,
     path: '/',
   },
+]
+
+export const router = createBrowserRouter([
+  {
+    children: privateRoutes,
+    element: <PrivateRoutes />,
+  },
+  ...publicRoutes,
 ])
 
-export const Router = () => {
-  return <RouterProvider router={router} />
+function PrivateRoutes() {
+  const isAuthenticated = false
+
+  return isAuthenticated ? <Outlet /> : <Navigate to={'/login'} />
 }
